de35dfc7e24429df3bb6cd65eb8d9c971c0697c0
dedane <evanskeema@gmail.com>
2021-10-31 16:44:49 +0300

    creating-smart-contract

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,33 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { isTargetLikeServerless } = require("next/dist/server/config");
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  it("Should created and execute Market sales", async function () {
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.marketAddress
+    
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress)
+    await NFT.deployed();
+    const ndtContractAddress = nft.address;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let listingPrice = await market.getListingPrice();
+    listingPrice = listingPrice.toString()
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const auctionPrice = ethers.utils.parseUnits('100', 'ether')
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.mytokenlocation2.com")
+
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1,{value: auctionPrice});
+  })
 });
